90dd178b24f55f8d82b0713c7d1043c3e272c083
Vương Hồng Lĩnh <linhhandsom@gmail.com>
2024-06-06 16:39:32 +0700

    add vercel json

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import *
+from flask_cors import CORS, cross_origin
 import json
 
 app = Flask(__name__)
-
+CORS(app)
 
 def read_json():
     with open('input.json', 'r') as file:
@@ -12,12 +13,18 @@ def read_json():
 
 # endpoints
 @app.route('/',methods=['GET'])
+
 def index():
     return redirect('/schedules')
+
 @app.route('/schedules', methods=['GET'])
+
 def schedules():
-    data = read_json()
-    return jsonify(data)
+    data = jsonify(read_json())
+    data.headers.add('Access-Control-Allow-Origin', '*')
+    data.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+    data.headers.add('Access-Control-Allow-Methods', 'GET, POST')
+    return data
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
